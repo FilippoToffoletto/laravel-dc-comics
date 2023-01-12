446963a5d2a2a9daf3ef50ccf88308dfaf30d122
FilippoToffoletto <filippotoffoletto@hotmail.it>
2023-01-12 18:26:50 +0100

    [add]page detail texts

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -49,7 +49,8 @@ class ComicController extends Controller
     {
         //
         $comic = Comic::findOrFail($id);
-        dd($comic);
+
+        return view('comics.show', compact('comic'));
     }
 
     /**
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -3,10 +3,27 @@
 @section('content')
 
     <div class="container">
-        <h1>Dettaglio comic</h1>
+        <div class="row">
+            <div class="col-8">
+                <h1 class="py-2">{{$comic->title}}</h1>
+                <span class="fs-3 text-capitalize d-block py-1">Type:{!!$comic->type!!}</span>
+                <span class="fs-3 text-capitalize d-block py-1">Series:{!!$comic->series!!}</span>
+                <span class="fs-3 text-capitalize d-block py-1">Sale Date:{!!$comic->sale_date!!}</span>
+                <span class="fs-3 text-capitalize d-block py-1">${!!$comic->price!!}</span>
+                <p class="me-4 text-justify mt-3">{!!$comic->description!!}</p>
 
 
+                <!-- $new_comic->price= $comic_item['price'];
+                $new_comic->sale_date= $comic_item['sale_date'];
+                -->
 
+            </div>
+            <div class="col-4">
+                <img src="{{$comic->thumb}}" alt="{{$comic->title}}" class="py-2 w-75">
+            </div>
+
+
+        </div>
     </div>
 
 @endsection
