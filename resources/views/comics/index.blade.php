20f831ade34177db99b34a6194b02690393d5d59
FilippoToffoletto <filippotoffoletto@hotmail.it>
2023-01-12 17:53:27 +0100

    [add]table

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,5 +1,33 @@
 @extends('layouts.main')
 
 @section('content')
-    <h1>Elenco Comics</h1>
+
+    <div class="container">
+        <h1>Elenco Comics</h1>
+
+        <table class="table table-striped table-dark">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Tipo</th>
+                <th scope="col">Azioni</th>
+              </tr>
+            </thead>
+            <tbody>
+                @forelse ($comics as $comic)
+                <tr>
+                    <td>{{$comic->id}}</td>
+                    <td>{{$comic->title}}</td>
+                    <td>{{$comic->type}}</td>
+                    <td>xxxx</td>
+                  </tr>
+                @empty
+                  <h2>Nessun prodotto</h2>
+                @endforelse
+
+            </tbody>
+          </table>
+    </div>
+
 @endsection
